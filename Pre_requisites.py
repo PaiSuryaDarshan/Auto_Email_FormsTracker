b54416aa69aa76198cca737cabe24b38feba9105
PaiSuryaDarshan <paisuryadarshan.research@gmail.com>
2024-10-05 01:59:46 +0100

    update Main.py, Pre_requisites.py, README.md and Pre_requisites.cpython-312.pyc

--- Pre_requisites.py
+++ Pre_requisites.py
@@ -3,11 +3,11 @@
 # Standard Values
 email_sender   = "chemistryscgroup22@gmail.com"
 email_password = "xzep lgdr ovky wnjd"
-email_receiver = ["Suryadarshan82@gmail.com", "k22012568@kcl.ac.uk", "paisuryadarshan.research@gmail.com"]
+email_receiver = ["Suryadarshan82@gmail.com", "k22012568@kcl.ac.uk", "paisuryadarshan.research@gmail.com", "ridwaan03kkj@gmail.com"]
 
 # Variable Values
 subject = "Final SOLO test run"
 body = """
-New google form sheets | Test Form link: https://forms.gle/8j7kqTxNoKuU29Xf8
+New google form sheets
+Hello Ridwaan, this is Me sending an automated email | Test Form link: https://forms.gle/8j7kqTxNoKuU29Xf8
 """
-
